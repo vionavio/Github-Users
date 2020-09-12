bf726c2279690e92f0c5b1c683007387c0675672
Viona <vionamahdiya@gmail.com>
2020-09-12 22:25:41 +0700

    Add splashscreen

---
+++ app/src/main/java/com/vionavio/githubuser/splashscreen/Splashscreen.kt
@@ -0,0 +1,22 @@
+package com.vionavio.githubuser.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.vionavio.githubuser.R
+import com.vionavio.githubuser.view.MainActivity
+
+
+class Splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
